90388d882caee41a19fa197b317aed1f13b45a96
deepatel055 <deeppatel2gmail.com>
2025-07-04 19:44:36 +0530

    Chnages 1

--- Task-Management/src/services/api.js
+++ Task-Management/src/services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'http://localhost:5000/api',
-    // baseURL: 'https://project-management-vy7e.onrender.com',
   withCredentials: true, // 👈 send cookies with each request
 });
 
