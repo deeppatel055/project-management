7bcd7d686e51a34d9284a9b8b8b8c2aef2b1a2b6
Deep Patel <deeppatel53951@gmail.com>
2025-07-04 17:28:25 +0530

    Update api.js

--- Task-Management/src/services/api.js
+++ Task-Management/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://project-management-vy7e.onrender.com',
   withCredentials: true, // 👈 send cookies with each request
 });
 
